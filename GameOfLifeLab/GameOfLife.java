10c0c507785d69b02fff287eb62b0b8b7a8ff433
scottsmith3691 <spsmith@NN121-HA-17.HS.sd203.org>
2015-11-24 16:28:34 -0600

    11/24/15

--- GameOfLifeLab/GameOfLife.java
+++ GameOfLifeLab/GameOfLife.java
@@ -6,7 +6,7 @@ import info.gridworld.grid.BoundedGrid;
 import info.gridworld.grid.Location;
 import java.awt.Color;
 import java.util.ArrayList;
-
+import java.util.*;
 
 
 /**
@@ -20,7 +20,7 @@ public class GameOfLife
 {
     // the world comprised of the grid that displays the graphics for the game
     private ActorWorld world;
-    
+    private ActorWorld newWorld;
     // the game board will have 5 rows and 5 columns
     private final int ROWS = 8;
     private final int COLS = 8;
@@ -75,42 +75,34 @@ public class GameOfLife
         // create and add bugs (a type of Actor) to the three intial locations
         Bug bug1 = new Bug(Color.CYAN);
         Location loc1 = new Location(X1, Y1);
-        actors.add(new Object[]{bug1, loc1});
         grid.put(loc1, bug1);
         
         Bug bug2 = new Bug(Color.CYAN);
         Location loc2 = new Location(X2, Y2);
-        actors.add(new Object[]{bug2, loc2});
         grid.put(loc2, bug2);
         
         Bug bug3 = new Bug(Color.CYAN);
         Location loc3 = new Location(X3, Y3);
-        actors.add(new Object[]{bug3, loc3});
         grid.put(loc3, bug3);
         
         Bug bug4 = new Bug(Color.CYAN);
         Location loc4 = new Location(X4, Y4);
-        actors.add(new Object[]{bug4, loc4});
         grid.put(loc4, bug4);
         
         Bug bug5 = new Bug(Color.CYAN);
         Location loc5 = new Location(X5, Y5);
-        actors.add(new Object[]{bug5, loc5});
         grid.put(loc5, bug5);
         
         Bug bug6 = new Bug(Color.CYAN);
         Location loc6 = new Location(X6, Y6);
-        actors.add(new Object[]{bug6, loc6});
         grid.put(loc6, bug6);
         
         Bug bug7 = new Bug(Color.CYAN);
         Location loc7 = new Location(X7, Y7);
-        actors.add(new Object[]{bug7, loc7});
         grid.put(loc7, bug7);
         
         Bug bug8 = new Bug(Color.CYAN);
         Location loc8 = new Location(X8, Y8);
-        actors.add(new Object[]{bug8, loc8});
         grid.put(loc8, bug8);
     }
 
@@ -122,7 +114,7 @@ public class GameOfLife
      * @post    the world has been populated with a new grid containing the next generation
      * 
      */
-    private void createNextGeneration()
+    private void createNextGeneration()throws InterruptedException
     {
         /** You will need to read the documentation for the World, Grid, and Location classes
          *      in order to implement the Game of Life algorithm and leverage the GridWorld framework.
@@ -130,35 +122,42 @@ public class GameOfLife
         
         // create the grid, of the specified size, that contains Actors
         Grid<Actor> grid = world.getGrid();
-        
-        for(int i=1;i<COLS;i++)
+        BoundedGrid<Actor> newGrid = new BoundedGrid<Actor>(ROWS, COLS);
+        for(int i=0;i<COLS;i++)
         {
-            for( int x=1;x<=ROWS;x++)
+            for( int x=0;x<ROWS;x++)
             {
-                
-                
-                //if (grid
-                
-                
-                
-                
-                
-                
+                Location l= new Location(x,i);
+               
+                if(grid.get(l) != null)
+                {
+                   if (grid.getOccupiedAdjacentLocations(l).size()==2 
+                   || grid.getOccupiedAdjacentLocations(l).size()==3)
+                   {
+                       Bug nBug = new Bug(Color.CYAN);
+                       newGrid.put(l,nBug);
+                       
+  
+                   } 
+                   else
+                   {
+                       grid.get(l).setColor(Color.RED);
+                   }
+                }
+                if(grid.get(l) == null)
+                {
+                    if (grid.getOccupiedAdjacentLocations(l).size()==3)
+                    {
+                        Bug nBug = new Bug(Color.CYAN);
+                        newGrid.put(l,nBug);
+                    }
+                }
+                Thread.sleep(100);
+                }
             }
-            Location l = (Location)actors.get(i)[1];
-            System.out.println(l);
-            ArrayList<Location> neighbors = grid.getNeighbors(l);
-            System.out.println(grid.getNeighbors(l));
-            int size=neighbors.size();
-            if (size<2)
-            {
-                grid.remove(l);
-            }
-            else if( size >=2 && size<4)
-            {
-                
-            }
-        }
+        world.setGrid(newGrid);
+        world.show();
+        Thread.sleep(1000);
     }
     
     /**
@@ -201,10 +200,16 @@ public class GameOfLife
      * Creates an instance of this class. Provides convenient execution.
      *
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
-        GameOfLife game = new GameOfLife();
-        game.createNextGeneration();
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many generations would you like it to run through(1-8): ");
+        int gen = scan.nextInt();GameOfLife game = new GameOfLife();
+        for(int i = 1; i <= gen; i++)
+        {
+            game.createNextGeneration();
+        }
     }
 
 }
